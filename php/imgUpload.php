312c46915c21ee638f3729b90b44a287e91dca2b
halambique <mathias@mbq.no>
2024-02-08 12:59:09 +0100

    bildetest

--- php/imgUpload.php
+++ php/imgUpload.php
@@ -1,5 +1,5 @@
 <?php
-$target_dir = "img/"; // Ensure this directory exists in your project
+$target_dir = "../img/"; // Ensure this directory exists in your project
 $target_file = $target_dir . basename($_FILES["lecturerImage"]["name"]);
 
 if (move_uploaded_file($_FILES["lecturerImage"]["tmp_name"], $target_file)) {
