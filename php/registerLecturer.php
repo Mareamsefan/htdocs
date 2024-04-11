8ed60c806434c8113154be3c1e9b51392b1c1c6b
sirsa4 <yaya.abbdi@gmail.com>
2024-04-11 13:16:43 +0200

    image uploaded outside project root directory

--- php/registerLecturer.php
+++ php/registerLecturer.php
@@ -13,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $SubjectPIN = $_POST["SubjectPIN"];
 
     // File upload
-    $target_dir = "../img/";
+    $target_dir = "../../imges/";
     $target_file = $target_dir . basename($_FILES["lecturerImage"]["name"]);
 
     //file validation
